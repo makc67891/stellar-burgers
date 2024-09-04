e85539072c65e1006a4a9789b968c72eba7d7583
Maksim Mezenets <makc67891@yandex.ru>
2024-09-05 01:12:59 +0300

    константы

---
+++ cypress/constants.ts
@@ -0,0 +1,14 @@
+export const allBuns = '[data-cy=bun-ingredients]';
+export const bunTop = '[data-cy=constructor-bun-1]';
+export const bunBottom = '[data-cy=constructor-bun-2]';
+export const burgerConstructorMain = '[data-cy=constructor-main]';
+export const allMain = '[data-cy=main-ingredients]';
+export const allSause = '[data-cy=sauces-ingredients]';
+export const burgerConstructor = '[data-cy=constructor]';
+export const ingredientBun = 'Ингредиент 1';
+export const ingredientMain = 'Ингредиент 2';
+export const ingredientSause = 'Ингредиент 4';
+export const orderNumber = '[data-cy=order-number]';
+export const orderButton = '[data-cy=order-button] button'
+export const closeButton = '[data-cy=close-modal]'
+export const ingredientAddButton = 'Добавить';
--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,3 +1,19 @@
+import {
+  allBuns,
+  allMain,
+  allSause,
+  bunBottom,
+  bunTop,
+  burgerConstructor,
+  burgerConstructorMain,
+  closeButton,
+  ingredientBun,
+  ingredientMain,
+  ingredientSause,
+  orderButton,
+  orderNumber
+} from 'cypress/constants';
+
 beforeEach(function () {
   cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
   cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
@@ -8,7 +24,7 @@ beforeEach(function () {
   );
   cy.setCookie('accessToken', 'test-accessToken');
   cy.viewport(1920, 1080);
-  cy.visit('http://localhost:4000');
+  cy.visit('/');
 });
 
 afterEach(function () {
@@ -18,45 +34,41 @@ afterEach(function () {
 
 it('Добавление булок', function () {
   // добавили первую булку
-  cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
+  cy.get(allBuns).contains('Добавить').click();
   // проверили наличие в конструкторе
-  cy.get('[data-cy=constructor-bun-1]')
-    .contains('Ингредиент 1')
-    .should('exist');
-  cy.get('[data-cy=constructor-bun-2]')
-    .contains('Ингредиент 1')
-    .should('exist');
+  cy.get(bunTop).contains(ingredientBun).should('exist');
+  cy.get(bunBottom).contains(ingredientBun).should('exist');
 });
 
 it('Добавление начинки', function () {
   // добавили первый ингредиент
-  cy.get('[data-cy=main-ingredients]').contains('Добавить').click();
+  cy.get(allMain).contains('Добавить').click();
   // проверили наличие в конструкторе
-  cy.get('[data-cy=constructor-main]').contains('Ингредиент 2').should('exist');
+  cy.get(burgerConstructorMain).contains(ingredientMain).should('exist');
 });
 
 it('Добавление соуса', function () {
   // добавили первый соус
-  cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
+  cy.get(allSause).contains('Добавить').click();
   // проверили наличие в конструкторе
-  cy.get('[data-cy=constructor-main]').contains('Ингредиент 4').should('exist');
+  cy.get(burgerConstructorMain).contains(ingredientSause).should('exist');
 });
 
 it('Оформление заказа', function () {
   // добавили игредиенты
-  cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
-  cy.get('[data-cy=main-ingredients]').contains('Добавить').click();
-  cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
+  cy.get(allBuns).contains('Добавить').click();
+  cy.get(allMain).contains('Добавить').click();
+  cy.get(allSause).contains('Добавить').click();
   // нажали на кнопку "оформить заказ"
-  cy.get('[data-cy=order-button] button').click();
+  cy.get(orderButton).click();
   // проверили номер заказа
-  cy.get('[data-cy=order-number]').contains('12345').should('exist');
+  cy.get(orderNumber).contains('12345').should('exist');
   // нажали на крестик для закрытия модалки
-  cy.get('[data-cy=close-modal]').click();
+  cy.get(closeButton).click();
   // убедились в закрытии модалки
-  cy.get('[data-cy=order-number]').should('not.exist');
+  cy.get(orderNumber).should('not.exist');
   // убедились в очистке конструктора
-  cy.get('[data-cy=constructor]').contains('Ингредиент 1').should('not.exist');
-  cy.get('[data-cy=constructor]').contains('Ингредиент 2').should('not.exist');
-  cy.get('[data-cy=constructor]').contains('Ингредиент 4').should('not.exist');
+  cy.get(burgerConstructor).contains(ingredientBun).should('not.exist');
+  cy.get(burgerConstructor).contains(ingredientMain).should('not.exist');
+  cy.get(burgerConstructor).contains(ingredientSause).should('not.exist');
 });
